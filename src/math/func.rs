59ab6fc33bf389260b41d2d6a99f09ab2bd674c2
SKKUGoon <baesangil0906@gmail.com>
2023-10-15 19:46:35 +0900

    Start working on function builder

--- src/math/func.rs
+++ src/math/func.rs
@@ -1,47 +1,25 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn placeholder() {}
-
-#[wasm_bindgen]
 #[derive(Debug, Clone)]
-pub struct FuncBuilder {
+pub struct FunctionBuilder {
     #[wasm_bindgen(getter_with_clone)]
-    pub operations: Box<[i32]>,
+    pub function: String,
+
+    pub value: i64,
 
     #[wasm_bindgen(getter_with_clone)]
     pub sheet_dependency: Box<[js_sys::JsString]>,
 }
 
-#[wasm_bindgen]
-pub struct Symbol {
-    pub key: i32,
-
-    #[wasm_bindgen(getter_with_clone)]
-    pub symbol: String,
-
-    #[wasm_bindgen(getter_with_clone)]
-    pub prefix: String,
-
-    #[wasm_bindgen(getter_with_clone)]
-    pub suffix: String,
-}
-
-#[wasm_bindgen]
-#[derive(Copy, Clone, Debug)]
-pub enum Arithmetic {
-    Addition = 0,
-    Subtraction = 1,
-    Multiplication = 2,
-    Division = 3,
-}
-
-#[wasm_bindgen]
-#[derive(Copy, Clone, Debug)]
-pub enum FuncFmt {
-    Bracket = 0,
-    FuncOut = 1,
+impl PartialEq for FunctionBuilder {
+    fn eq(&self, other: &Self) -> bool {
+        self.function == other.function
+    }
 }
 
 // #[wasm_bindgen]
-// pub enum
+// impl FunctionBuilder {
+//     #[wasm_bindgen(constructor)]
+//     pub fn new<T>(starting_value: T) {}
+// }
